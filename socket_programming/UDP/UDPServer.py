ff1927dc26672efad2c4c16cc32f72e20979b1f6
root <root@localhost.localdomain>
2018-08-10 22:02:02 +0200

    extended functionality on server

--- socket_programming/UDP/UDPServer.py
+++ socket_programming/UDP/UDPServer.py
@@ -1,13 +1,21 @@
 from socket import *
 
-serverPort = input('type port which server should listen on')
+serverPort = int(input('type port which server should listen on: '))
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
+# assigns the user applied port number to the server's socket
 serverSocket.bind(('', serverPort))
 
 print("Server is running ...")
 
 while True:
 	message, clientAddress = serverSocket.recvfrom(2048)
+	
+	# clientAddress is a tuple with (host, port)
+	print('from client ' 
+	+ clientAddress[0] 
+	+ ':' + str(clientAddress[1]) 
+	+ ' ' + message.decode())
+
 	modifiedMessage = message.decode().upper()
 	serverSocket.sendto(modifiedMessage.encode(), clientAddress)
